7f05d309a7ad74b2d1b254f2816f1255565751d8
husyy003 <31714898+husyy003@users.noreply.github.com>
2018-06-04 18:41:19 +0930

    Putting threads of new created players in hashmap
    Thread of Players can be obtained using hashmap

--- src/Server.java
+++ src/Server.java
@@ -11,26 +11,95 @@ public class Server {
 	
 	private Buffer buffer;
 	private State gameState;
-	private HashMap playerStates;
-	static HashMap<String, Player> hmap = new HashMap<String, Player>();
+	
+	//hashmap / looby to store the values of player name 
+	//and their corresponding thread
+	static HashMap<String, Thread> hmap = new HashMap<String, Thread>();
+	//map database where all the records are saved
 	static File dbFile = Utils.tempDbFile();
 	static DB db = DBMaker.newFileDB(dbFile)
 			.closeOnJvmShutdown()
 			.encryptionEnable("password")
 			.make();
+//open an collection, TreeMap has better performance then HashMap
+	static ConcurrentNavigableMap<Integer,String> players = db.getTreeMap("Players");
+	static ConcurrentNavigableMap<String,Integer> passwords = db.getTreeMap("Passwords");
 
-	//open an collection, TreeMap has better performance then HashMap
-	static ConcurrentNavigableMap<Integer,String> map = db.getTreeMap("collectionName");
 
-	//CONSTRUCTOR
-	public Server () {
+	//**CONSTRUCTOR
+		public Server () {
 
+		}
+		
+		
+		//Create 100 players and their 100 passwords and puts them in database(mapDB) **=>
+		public synchronized static void LoginData()
+		{
+			for(int i=0;i<100;i++)
+			{
+				players.put(i,"Player"+i);
+				passwords.put("Player"+i,i);
+			}
+			db.commit();  //persist changes into disk
+		}
+		
+		//**<=
+		
+	//verify login player **=>
+	public synchronized static boolean login (Player player, int password) {
+		if(getPlayer(player.getPlayerName(),password))
+		{
+			Thread playerR=new Thread(new Player(player.getPlayerName(),password));
+			putPlayerInHashMap(playerR);
+			System.out.println("putting player in hashMap, Name: "+player.getPlayerName());
+			return true;
+		}
+		return false;
 	}
+	
+	// **<=
+	
+	//checks if player exists in record and
+	//returns true if it does **=>
+	public static boolean getPlayer(String name, int password)
+	{
+		for(int i=0;i<100;i++)
+		{
+			//player index in players database and his password index 
+			//in passwords is same so we are checking if player and their password matches
+			if(players.get(i).equals(name) && password==i)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// **<=
+	
+	//puts player in hashmap if login was successful **=>
+	public static void putPlayerInHashMap(Thread player)
+	{
+			hmap.put(player.getName(),player);
+	}
+	
+	//<=
+	
+	
+
+	
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Hi this is a temp main method in server");
-		System.out.println("Creating new state :)");
-		State newState = new State(0, 0);
+		//Create 100 player's data with passwords
+		LoginData();
+		for(int i = 0;i<100;i++)
+		{
+			Player player= new Player("Player"+i,i);
+		login(player,i);
+		}
+		
 	}
 	
 	//pull moves from the buffer
@@ -57,66 +126,11 @@ public class Server {
 
 
 	private synchronized void removePlayer (Player removePlayer) {
-		playerStates.remove(removePlayer);
+		hmap.remove(removePlayer);
 	}
 
 
 
-	//*
-	//verify login player
-	public synchronized static boolean login (Player loginPlayer) {
-		System.out.println("This is player name"+loginPlayer.getPlayerName());
-		if(getPlayer(loginPlayer.PlayerName))
-		{
-			putPlayerInHashMap(loginPlayer);
-			System.out.println("The player exists in record");
-			return true;
-		}
 
-
-		return false;
-	}
-	
-	
-
-	//checks if player exists in record and returns true if it does
-	public static boolean getPlayer(String name)
-	{
-		for(int i=0;i<100;i++)
-		{
-			if(map.get(i).equals(name))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
-	//puts player in hashmap
-	public static void putPlayerInHashMap(Player p1)
-	{
-		if(getPlayer(p1.getPlayerName()))
-		{
-			hmap.put(p1.getPlayerName(),p1);
-		}
-	}
-	
-	
-	//Create 100 players and puts them in mapDB
-	public static void LoginData()
-	{
-		for(int i=0;i<100;i++)
-		{
-			map.put(i,"Player"+i);
-		}
-		//map.keySet() is now [1,2] even before commit
-
-		db.commit();  //persist changes into disk
-
-		//map.keySet() is now [1,2,3]
-		db.rollback(); //revert recent changes
-		//map.keySet() is now [1,2]
-		System.out.println(map.get(1));
-	}
 
 }
