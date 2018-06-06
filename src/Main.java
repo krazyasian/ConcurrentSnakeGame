e18f67b108cf52b806230c37d9c7132ae2d7db45
Albert Palada <palap002@mymail.unisa.edu.au>
2018-06-07 02:40:39 +0930

    Modified the code so each player will be running on its own thread
    Modified the code so each player will be running on its own thread

--- src/Main.java
+++ src/Main.java
@@ -42,29 +42,39 @@ public class Main {
 		gameServer.getGameState().populate(playerList, playerList.size());
 		gameServer.getGameState().render();
 
-		while(gameServer.isRunning())
-		{
-			Thread.sleep(200);
+//		while(gameServer.isRunning())
+//		{
+//			
 			for (int j=players; j<playerList.size();j++)
 			{
 				final int a = j;
-				
-				if(playerList.get(a).getAlive()) {
+
 					Thread thread = new Thread(){
 					    public void run(){
-					    	gameServer.getBuffer().append(a, (int) Math.floor(Math.random()*4)+1);
-							gameServer.updateGameInterface();
+					    	while(gameServer.isRunning()) {
+					    		try {
+									Thread.sleep(200);
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+					    		if(playerList.get(a).getAlive()) {
+						    		gameServer.getBuffer().append(a, (int) Math.floor(Math.random()*4)+1);
+									gameServer.updateGameInterface();
+						    	}
+					    	}
+					    	
 					    }
 					  };
 					
 					thread.start();
-				}	
+
 				
 //				playerList.replace(j, gameServer.getGameState().move(playerList.get(j), (int) Math.floor(Math.random()*4)+1));
 			}
 
 //			gameServer.getGameState().render();
-		}
+//		}
 //		
 //		//Testing 
 //		String moveAndPlayerID=
